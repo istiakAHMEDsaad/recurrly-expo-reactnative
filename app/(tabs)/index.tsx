92143b4738d6abeb1a539b49f78caccb27f30988
istiakAHAMMEDsaad <istik2020@gmail.com>
2026-09-03 20:44:29 +0600

    ...

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,9 +8,7 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function Index() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
+      <Text className="font-sans-extrabold text-5xl">Home</Text>
 
       <Link
         href="/(auth)/sign-in"
@@ -23,13 +21,6 @@ export default function Index() {
         className="mt-4 rounded bg-primary text-white p-4"
       >
         Sign up
-      </Link>
-
-      <Link href="/subscription/hululu">hululu subscription</Link>
-      <Link
-        href={{ pathname: "/subscription/[id]", params: { id: "deepseek" } }}
-      >
-        Deepseek
       </Link>
     </SafeAreaView>
   );
